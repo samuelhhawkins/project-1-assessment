ee1245ccbcf1015bf280b09ef697da53caa93c5f
samuelhhawkins <samuel.h.hawkins@hotmail.com>
2020-04-06 16:27:49 -0700

    finished test

--- main.js
+++ main.js
@@ -6,16 +6,28 @@ let currVal = 0
 
 add.addEventListener('click', () => {
     let adder = parseInt(input.value)
+    if(input.value == 0){
+      adder = 1
+    }
     currVal += adder
     display.innerHTML = currVal
-
+    changeColor()
 });
 
 subtract.addEventListener('click', () => {
   let sub = parseInt(input.value)
+  if(input.value == 0){
+    sub = 1
+  }
   currVal -= sub
   display.innerHTML = currVal
+  changeColor()
+});
+
+function changeColor() {
   if(currVal < 0){
     display.style.color = 'red'
+  } else{
+    display.style.color = 'black'
   }
-});
+}
